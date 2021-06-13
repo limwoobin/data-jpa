179ec85326380398223296895e316539aca028f8
limwoobin <drogba02@naver.com>
2021-06-13 15:25:39 +0900

    김영한 Spring Data Jpa
    - 순수 Jpa 페이징 , 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -41,4 +41,18 @@ public class MemberJpaRepository {
     public void delete(Member member) {
         em.remove(member);
     }
+
+    public List<Member> findByPage(int age , int offset , int limit) {
+        return em.createQuery("select m from Member m where age = :age order by m.username desc")
+                .setParameter("age" , age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age" , Long.class)
+                .setParameter("age" , age)
+                .getSingleResult();
+    }
 }
