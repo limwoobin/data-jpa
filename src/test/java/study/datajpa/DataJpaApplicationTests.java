ba6a52df394cb5107adb06294381d43e253a543d
limwoobin <drogba02@naver.com>
2021-06-17 20:02:44 +0900

    김영한 Spring Data Jpa + QueryDsl
    - QueryDsl 세팅

--- src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -1,13 +1,22 @@
 package study.datajpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class DataJpaApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
