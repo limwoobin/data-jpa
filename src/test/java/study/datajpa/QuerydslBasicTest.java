1d1cf5ca7375e3fe1e3ce2a3cc8f5e29b6e70af7
limwoobin <drogba02@naver.com>
2021-06-19 15:17:50 +0900

    김영한 Spring Data Jpa + QueryDsl
    - QueryDsl case , 상수처리  , concat

--- src/test/java/study/datajpa/QuerydslBasicTest.java
+++ src/test/java/study/datajpa/QuerydslBasicTest.java
@@ -2,6 +2,8 @@ package study.datajpa;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -405,6 +407,63 @@ public class QuerydslBasicTest {
             System.out.println("tuple.get(0 , Member.class) = " + tuple.get(0 , Member.class));
             System.out.println("tuple.get(1 , Member.class) = " + tuple.get(1 , Member.class));
             System.out.println("======================================================");
+        }
+    }
+
+    @Test
+    public void basicCase() {
+        List<String> result = queryFactory
+                .select(member.age
+                    .when(10).then("열살")
+                    .when(20).then("스무살")
+                    .otherwise("기타")
+                ).from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void complexCase() {
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                    .when(member.age.between(0 , 20)).then("0 ~ 20 살")
+                    .when(member.age.between(21 , 30)).then("21 ~ 30 살")
+                    .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat() {
+        List<String> result = queryFactory
+                .select(member.username.concat("_")
+                        .concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+
         }
     }
 }
