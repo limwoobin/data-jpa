9e52f76235f9ae98ac078ae5982cefe277d3df9a
limwoobin <drogba02@naver.com>
2021-06-19 14:37:56 +0900

    김영한 Spring Data Jpa + QueryDsl
    - QueryDsl Join

--- src/test/java/study/datajpa/QuerydslBasicTest.java
+++ src/test/java/study/datajpa/QuerydslBasicTest.java
@@ -213,4 +213,94 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
+
+    /**
+    * 팀 A 에 소속된 모든 회원
+    * */
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team , team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1" , "member2");
+    }
+
+    @Test
+    public void join2() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(team).on(member.team.id.eq(team.id))
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1" , "member2");
+    }
+
+    /**
+    *   세타 조인
+    *   회원의 이름이 팀 이름과 같은 회원
+    * */
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member , team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA" , "teamB");
+    }
+
+    /**
+    * JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+    * */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team , team)
+//                .on(team.name.eq("teamA"))    where 과 동일
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     *   연관관계가 없는 엔티티 외부 조인
+     *   회원의 이름과 팀 이름이 같은 대상 외부 조인
+     * */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member , team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+//                .innerJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
