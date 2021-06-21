651e90277bb5b9c46457305c179a4c3cab328964
woobeen <drogba02@naver.com>
2021-06-21 19:56:43 +0900

    김영한 Spring Data Jpa + QueryDsl
    - 조회 컨트롤러 개발 #1

---
+++ src/main/java/study/datajpa/controller/MemberDslController.java
@@ -0,0 +1,21 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberSearchCondition;
+import study.datajpa.dto.MemberTeamDto;
+import study.datajpa.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberDslController {
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
