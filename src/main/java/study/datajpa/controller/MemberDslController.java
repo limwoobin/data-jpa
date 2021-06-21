7eb17c81e4668aad136c65ff344a773bef0d6058
limwoobin <drogba02@naver.com>
2021-06-22 04:52:36 +0900

    김영한 Spring Data Jpa + QueryDsl
    - 조회 컨트롤러 개발 #2

--- src/main/java/study/datajpa/controller/MemberDslController.java
+++ src/main/java/study/datajpa/controller/MemberDslController.java
@@ -1,10 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.datajpa.dto.MemberSearchCondition;
 import study.datajpa.dto.MemberTeamDto;
+import study.datajpa.repository.dsl.MemberDslRepository;
 import study.datajpa.repository.dsl.MemberJpaRepository;
 
 import java.util.List;
@@ -13,9 +16,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberDslController {
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberDslRepository memberDslRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition , Pageable pageable) {
+        return memberDslRepository.searchPageSimple(condition , pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition , Pageable pageable) {
+        return memberDslRepository.searchPageComplex(condition , pageable);
     }
 }
