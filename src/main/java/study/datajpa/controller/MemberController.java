c6f45dbebf13f1adaa6d23cfe7d7d94589865795
limwoobin <drogba02@naver.com>
2021-07-07 00:50:02 +0900

    김영한 Spring Data Jpa + QueryDsl
    - Transactional 없이 CUD 테스트 (Application)

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
@@ -17,6 +15,36 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberRepository memberRepository;
+
+    @PutMapping("/member/insert/{name}")
+    public String save(@PathVariable("name") String name) {
+        Member member = new Member(name);
+        memberRepository.save(member);
+        return "OK";
+    }
+
+    @PostMapping("/member/update/{name}")
+    public String update(@PathVariable("name") String name) {
+        Member member = new Member(name);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(1L).get();
+        findMember.setUsername("zzzzz");
+        memberRepository.save(findMember);
+
+        return "OK";
+    }
+
+    @DeleteMapping("/member/delete/{name}")
+    public String delete(@PathVariable("name") String name) {
+        Member member = new Member(name);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(1L).get();
+        memberRepository.delete(findMember);
+
+        return "OK";
+    }
 
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id") Long id) {
--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -3,6 +3,7 @@ package study.datajpa.entity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.repository.MemberRepository;
@@ -16,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
+//@Transactional
 class MemberTest {
 
     @PersistenceContext
